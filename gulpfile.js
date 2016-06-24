6c46690c40afae6f1dcc9dd2b134c741ee804b20
Filippo Masoni <f.masoni@hotmail.it>
2016-06-24 22:26:14 +0200

    layout templating

--- gulpfile.js
+++ gulpfile.js
@@ -98,7 +98,7 @@ gulp.task('js', function() {
 gulp.task('watch', function () {
     gulp.watch('_assets/js/*.js', ['js']).on("change", browserSync.reload);
     gulp.watch('_assets/css/**', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.md','*.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
     gulp.watch('_jadefiles/*.jade', ['jade']);
 });
 
